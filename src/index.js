a047380386fa9b486ffea44dfec507265ebc8a37
Oscar Alviarez <oscalviarez@gmail.com>
2022-12-13 00:08:38 -0300

    change all to auth0

--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { getConfig } from "./config";
+import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+  //onRedirectCallback,
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <Auth0Provider {...providerConfig}>
+        <App />
+      </Auth0Provider>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+serviceWorker.unregister();
+reportWebVitals();
